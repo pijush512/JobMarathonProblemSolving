7ee7e36067972c8778aaa09f3da5d40598e8681b
pijush512 <psagorsarker512@gmail.com>
2026-06-10 00:36:58 +0600

    implement character occurrence counter using a clean for-loop

---
+++ Day-2/problem10.js
@@ -0,0 +1,16 @@
+// Problem 10: Count Occurrences of a Character  [Easy]
+// Description: Write a function countChar(str, char) that returns how many times a character appears in a string.
+// Example:
+// Input: 'banana', 'a'  → Output: 3
+// Hint: Use split(char).length - 1 or a loop.
+
+function countChar (str, char) {
+  let count = 0;
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === char){
+      count++;
+    }
+  }
+  return count;
+}
+console.log(countChar ('banana', 'a'));
