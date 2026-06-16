e48c0ac5e163cdf26d548796b19fd6b943e44f4d
pijush512 <psagorsarker512@gmail.com>
2026-06-16 16:35:56 +0600

    Solve Problem 33: Implement deepClone function using recursion to handle nested objects

---
+++ Day-7/problem33.js
@@ -0,0 +1,31 @@
+// Problem 33: Deep Clone an Object  [Medium]
+// Description: Write a function deepClone(obj) that returns a deep copy of a plain object without using JSON.parse/JSON.stringify.
+// Example:
+// const a = {x: {y: 1}};const b = deepClone(a);b.x.y = 99;// a.x.y is still 1
+// Hint: Use recursion and check for object/array types.
+
+
+function deepClone (obj) {
+ if (typeof obj !==  "object" || obj === null){
+  return obj;
+ }
+
+ const clone = Array.isArray(obj) ? [] : {};
+
+ for(let key in obj) {
+  clone[key] = deepClone (obj[key]);
+ }
+
+ return clone;
+}
+
+const a = {x: {y: 1}};
+const b = deepClone(a);
+b.x.y = 99;
+
+
+
+
+console.log("Original 'a':", a.x.y);
+
+console.log("Original 'a':", b.x.y);
