e3ebaca6a2501ccff89b7052ecd957cc249c0b5a
pijush512 <psagorsarker512@gmail.com>
2026-06-16 17:04:21 +0600

    Solve Problem 34: Implement EventEmitter class with on, emit, and off methods

---
+++ Day-7/problem35.js
@@ -0,0 +1,9 @@
+// Problem 35: Implement Array.prototype.map from Scratch  [Medium]
+// Description: Write a function myMap(arr, callback) that replicates the behavior of Array.prototype.map without using the built-in map().
+// Example:
+// myMap([1,2,3], x => x * 2);// Output: [2, 4, 6]
+// Hint: Loop through the array, apply the callback to each element, push results to a new array.
+
+function myMap (arr, callback) {
+  
+}
