b2367f30957e50ea33db6a3717e309d28f91ce83
pijush512 <psagorsarker512@gmail.com>
2026-06-16 17:18:27 +0600

    Solve Problem 35: Implement custom Array.prototype.map from scratch (Polyfill)

--- Day-7/problem35.js
+++ Day-7/problem35.js
@@ -4,6 +4,22 @@
 // myMap([1,2,3], x => x * 2);// Output: [2, 4, 6]
 // Hint: Loop through the array, apply the callback to each element, push results to a new array.
 
+
+// Problem 35: Implement Array.prototype.map from Scratch  [Medium]
+
 function myMap (arr, callback) {
-  
+  const result = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    const newValue = callback(arr[i], i, arr); 
+    result.push(newValue); 
+  }
+
+  return result; 
 }
+
+const nums = [1, 2, 3];
+const doubled = myMap(nums, x => x * 2);
+
+console.log("Original Array:", nums);  
+console.log("Transformed Array:", doubled); 
